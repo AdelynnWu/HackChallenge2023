aa9972f93bf9d8fd12c9b126bc34779f7c48ed94
Kathy Chen <kc994@cornell.edu>
2023-11-20 22:16:10 -0500

    make tab bar pink

--- HackChallenge2023/Controllers/TabController.swift
+++ HackChallenge2023/Controllers/TabController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class TabController: UITabBarController {
     override func viewDidLayoutSubviews() {
         var tabFrame = tabBar.frame
@@ -19,16 +20,17 @@ class TabController: UITabBarController {
         super.viewDidLoad()
         self.setupTabs()
         
-        self.tabBar.barTintColor = .red
-        self.tabBar.tintColor = UIColor.magenta
+
+        self.tabBar.tintColor = UIColor.systemPink
         self.tabBar.isTranslucent = false
-        self.tabBar.backgroundColor = UIColor.purple
+        //self.tabBar.backgroundColor = UIColor.purple
+        self.tabBar.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
     }
     
     private func setupTabs(){
-        let home = self.createNav(with: "Home", and: UIImage(systemName: "house"), vc: FeedVC())
-        let profile = self.createNav(with: "Profile", and: UIImage(systemName: "person.crop.circle"), vc: ProfileVC())
+        let home = self.createNav(with: "Organizations", and: UIImage(systemName: "person.3.fill"), vc: FeedVC())
+        let profile = self.createNav(with: "Saved", and: UIImage(systemName: "star"), vc: ProfileVC())
         let post = self.createNav(with: "Post", and: UIImage(systemName: "square.and.pencil.circle"), vc: PostAppVC())
 //        let vc = FeedVC()
 //        vc.tabBarItem.image
