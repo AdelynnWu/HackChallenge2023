2d2bf84bcb94202113f93dd90a402d72044f8eb5
Kathy Chen <kc994@cornell.edu>
2023-11-30 22:31:20 -0500

    fixing the stars

--- HackChallenge2023/Controllers/ProfileVC.swift
+++ HackChallenge2023/Controllers/ProfileVC.swift
@@ -218,6 +218,7 @@ extension ProfileVC: UICollectionViewDelegate {
         let detailedVC = DetailedVC(club_name: selectedItem.club_name,description:selectedItem.description, month: selectedItem.month, day: selectedItem.day, year: selectedItem.year, app_link: selectedItem.app_link, club_link: selectedItem.club_link,image_link:selectedItem.image_link,second_image_link: selectedItem.second_image_link, chat_link: selectedItem.chat_link,minute: selectedItem.minute, hour: selectedItem.hour,category: selectedItem.category,club: selectedItem)
         navigationController?.pushViewController(detailedVC, animated: true)
 //        clubCollectionView.reloadData()
+        detailedVC.delegate = self
     }
     
 }
